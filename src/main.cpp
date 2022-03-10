8bda07a5f38435193e1fbe70c1d88a9a259fc1a3
Jacob Malone <jcmalone1123@gmail.com>
2022-03-10 00:54:46 -0500

    Cleaned up code in main function.

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,7 @@
 #define MEMDUMP false
 #define DEBUG false
 
-//const std::string romFile = "roms/space_invaders.ch8";
-/*
-const int SCALING = 8;
-const uint32_t BG = 0x0;
-const uint32_t FG = 0xFFFFFF;
-*/
 const uint32_t frameTime = 2;
-/*
-int keys[16] = {
-    SDLK_x, SDLK_1, SDLK_2, SDLK_3,
-    SDLK_q, SDLK_w, SDLK_e, SDLK_a,
-    SDLK_s, SDLK_d, SDLK_z, SDLK_c,
-    SDLK_4, SDLK_r, SDLK_f, SDLK_v
-};
-*/
 
 int main (int argc, char* argv[]) {
 
@@ -47,31 +33,7 @@ int main (int argc, char* argv[]) {
     uint32_t last_time;
     uint32_t elapsed_time;
 
-    /*
-    SDL_Window* window = NULL;
-    SDL_Surface* surface = NULL;
-    SDL_Surface* frameBuffer = NULL;
-
-    if (SDL_Init(SDL_INIT_VIDEO) > 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    }
-    else {
-        window = SDL_CreateWindow("Chip-8 Emulator", SDL_WINDOWPOS_UNDEFINED,
-                                SDL_WINDOWPOS_UNDEFINED, 64 * SCALING,
-                                32 * SCALING, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-            return 1;
-        }
-        else {
-            surface = SDL_GetWindowSurface(window);
-            frameBuffer = SDL_GetWindowSurface(window);
-        }
-    }
-    */
-
     bool quit = false;
-    //SDL_Event e;
     last_time = SDL_GetTicks();
 
     while(!quit) {
@@ -80,32 +42,6 @@ int main (int argc, char* argv[]) {
 
         //Handle SDL Events (Keyboard)
         screen.handleInput(chip8.keyboard);
-        /*
-        while(SDL_PollEvent(&e)) {
-            if(e.type == SDL_QUIT) {
-                return 0;
-            }
-            if(e.type == SDL_KEYDOWN) {
-                if(e.key.keysym.sym == SDLK_ESCAPE) {
-                    return 0;
-                }
-                for(int i = 0; i < 16; ++i) {
-                    if(e.key.keysym.sym == keys[i]) {
-                        chip8.keyboard[i] = 1;
-                    }
-                }
-
-            }
-            if(e.type == SDL_KEYUP) {
-                for(int i = 0; i < 16; ++i) {
-                    if(e.key.keysym.sym == keys[i]) {
-                        chip8.keyboard[i] = 0;
-                    }
-                }
-
-            }
-        }
-        */
 
         chip8.emulateCycle();
         if(chip8.endEmulation())
@@ -115,22 +51,6 @@ int main (int argc, char* argv[]) {
         if(chip8.drawFlag){
             chip8.drawFlag = false;
             screen.draw(chip8.screenBuffer);
-            /*
-            SDL_FillRect(frameBuffer, NULL, BG);
-            SDL_Rect pixel;
-            for(int y = 0; y < 32; ++y) {
-                for(int x = 0; x < 64; ++x) {
-                    if(chip8.screenBuffer[y][x] == 1) {
-                        pixel.x = x * SCALING;
-                        pixel.y = y * SCALING;
-                        pixel.w = pixel.h = SCALING;
-                        SDL_FillRect(frameBuffer, &pixel, FG);
-                    }
-                }
-            }
-            SDL_BlitSurface(frameBuffer, NULL, surface, NULL);
-            SDL_UpdateWindowSurface(window);
-            */
         }
 
         if(DEBUG) {
@@ -142,11 +62,6 @@ int main (int argc, char* argv[]) {
         }
 
     }
-    /*
-    SDL_FreeSurface(frameBuffer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    */
     screen.close();
     return 0;
 }
